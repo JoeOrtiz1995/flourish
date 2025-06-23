0fd44f485c8935be67d089180926ddb7027137e4
Joe <Se_sema_95@hotmail.com>
2025-06-23 18:45:23 +0100

    Amend home file and add to ALLOWED_HOSTS

--- flourish_project/settings.py
+++ flourish_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-520&s$xx2f73z6g27kofnhg)061#4gt=qv=(o+@$b115rmqcmx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
