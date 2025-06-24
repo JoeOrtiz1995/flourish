919d61179ea8639f9fdea9076d4f1c8eaaf334d5
Joe <Se_sema_95@hotmail.com>
2025-06-24 19:33:37 +0100

    Amend home.html and styling

--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    """
+    A view to return the index page.
+    """
+    return render(request, 'home/index.html')
